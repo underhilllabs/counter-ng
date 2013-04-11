5ff0e9610ff078ece9d862d340a58271a7b57f9a
Bart Lantz <underhilllabs@gmail.com>
2013-04-10 22:13:32 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Counter-ng
+
+a Counter webapp written with HTML5 and Angular.js
---
+++ js/app.js
@@ -0,0 +1,7 @@
+this.CounterCtrl = function($scope) {
+  $scope.counter = 0;
+
+  $scope.inc = function() {
+    $scope.counter += 1;
+  }
+}
